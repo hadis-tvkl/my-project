e781e0e1c15ed5fa333e70ffdc8921174eae19fb
hadis-tvkl <htaquarious@gmail.com>
2022-06-12 13:08:14 +0330

    p

--- main.cpp
+++ main.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include "Jungle.h"
 #include "Animal.h"
+#include "WildAnimal.h"
+#include "DomesticAnimal.h"
+
 using namespace std;
 
 int main() {
-    Jungle wildsObject , domesticsObject;
     int n;
     cout<<"Enter the total number of animals";
     cin>>n;
-    wildsObject(n/2);
-    domesticsObject(n/2);   // now the animals are made
-
+    Jungle wildsObject(n/2) ;
+    Jungle domesticsObject(n/2);
+   // now the animals are made
+    WildAnimal* tempObject1;
     for (int i = 0; i < n/2; ++i) {
-        WildAnimal* tempObject;
-        wildsObject.AddAnimal(tempObject);  //animals are becoming added and customized successfully
-
+        wildsObject.AddAnimal(tempObject1);  //animals are becoming added and customized successfully
+    }
+    DomesticAnimal* tempObject2;
+    for (int i = 0; i < n/2; ++i) {
+        domesticsObject.AddAnimal(tempObject2);  //animals are becoming added and customized successfully
+    }
+    for (int i = 0; i < n/2; ++i) {
         for (int j = 0; j < 1000; ++j) {
             wildsObject.movement();
-        }
-    }
+        }}
     cout<<"The total fall of wild animals:\t"<<wildsObject.getTotalFall();
 
     for (int i = 0; i < n/2; ++i) {
-        DomesticAnimal* tempObject;
-        domesticObject.AddAnimal(tempObject);  //animals are becoming added and customized successfully
-
         for (int j = 0; j < 1000; ++j) {
             domesticsObject.movement();
         }
     }
-    cout<<"The totalfall of domestic animals:\t"<<domesticsObject.getTotalFall();
+    cout<<"\nThe total fall of domestic animals:\t"<<domesticsObject.getTotalFall();
 
     return 0;
 }
